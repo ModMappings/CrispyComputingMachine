917ad2f82d0278b63179845d8f5b21162ab3d243
Marc Hermans <marc.hermans@ldtteam.com>
2020-02-09 12:27:59 +0100

    Add initial spring boot structure.

---
+++ src/main/java/org/modmappings/crispycomputingmachine/CrispyComputingMachineApplication.java
@@ -0,0 +1,13 @@
+package org.modmappings.crispycomputingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrispyComputingMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrispyComputingMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/modmappings/crispycomputingmachine/CrispyComputingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.modmappings.crispycomputingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrispyComputingMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
